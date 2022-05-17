f41500e7c4baf5cbf32ac0decb3fc28c96ab0dab
mimura15 <102932331+mimura15@users.noreply.github.com>
2022-05-17 14:19:27 +0000

    [App] items/new

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,34 @@
 class Admin::ItemsController < ApplicationController
+	def index
+		
+	end
+	
+	def new
+		@item = Item.new
+	end
+	
+	def create
+		item = Item.new(item_params)
+		item.save
+		redirect_to admin_items_path
+	end
+	
+	def show
+		
+	end
+	
+	def edit
+		
+	end
+	
+	def update
+		
+	end
+	
+	private
+	
+	def item_params
+		params.require(:item).permit(:image, :name, :introduction, :price)
+	end
+	
 end
