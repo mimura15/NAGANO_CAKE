73d9f139723fcb001b3d59d77e1d15292aa2bd8c
mimura15 <102932331+mimura15@users.noreply.github.com>
2022-05-17 05:25:49 +0000

    [App] devise

--- db/migrate/20220516134937_devise_create_customers.rb
+++ db/migrate/20220516134937_devise_create_customers.rb
@@ -34,6 +34,15 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
 
 
       t.timestamps null: false
+      
+      t.string :last_name
+      t.string :first_name
+      t.string :last_name_kana
+      t.string :first_name_kana
+      t.string :postal_code
+      t.string :address
+      t.string :telephone_number
+      t.boolean :is_active, default: false
     end
 
     add_index :customers, :email,                unique: true
