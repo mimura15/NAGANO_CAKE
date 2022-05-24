c3dc91afae5de5c055e3f75d76fd1b7185a3ac3c
mimura15 <102932331+mimura15@users.noreply.github.com>
2022-05-24 08:09:09 +0000

    [Uproad] customer バリデーション

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -9,8 +9,12 @@ class Public::CustomersController < ApplicationController
 	
 	def update
 		customer = current_customer
-		customer.update(customer_params)
-		redirect_to customers_my_page_path
+		if customer.update(customer_params)
+			redirect_to customers_my_page_path
+		else
+			@customer = current_customer
+			render :edit
+		end
 	end
 	
 	def unsubscribe
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,4 +6,15 @@ class Customer < ApplicationRecord
          
   has_many :orders
   has_many :addresses
+  
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
+  validates :postal_code, presence: true
+  validates :address, presence: true
+  validates :telephone_number, presence: true
+  
 end
