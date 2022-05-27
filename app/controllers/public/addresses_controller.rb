3866818079363fa71450a67067508874b0e4f57f
mimura15 <102932331+mimura15@users.noreply.github.com>
2022-05-27 00:36:46 +0000

    [Add] 配送先登録

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,6 +1,7 @@
 class Public::AddressesController < ApplicationController
 	def index
-		
+		@address = Address.new
+		@addresses = Address.all
 	end
 	
 	def edit
@@ -8,7 +9,13 @@ class Public::AddressesController < ApplicationController
 	end
 	
 	def create
-		
+		address = Address.new(address_params)
+		if address.save
+			redirect_to addresses_path
+		else
+			@addresses = Addresses.all
+			render :index
+		end
 	end
 	
 	def update
@@ -17,6 +24,12 @@ class Public::AddressesController < ApplicationController
 	
 	def destroy
 		
+	end
+	
+	private
+	
+	def address_params
+		params.require(:address).permit(:postal_code, :address, :name)
 	end
 	
 end
