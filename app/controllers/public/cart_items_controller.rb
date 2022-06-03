0b80c57735ac87900ea11bb124eb99156d1a401d
mimura15 <102932331+mimura15@users.noreply.github.com>
2022-06-03 02:42:38 +0000

    [Add] カート画面

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -16,7 +16,15 @@ class Public::CartItemsController < ApplicationController
 	end
 	
 	def create
-		
+		@cart_item = Cart_item.new(cart_item_params)
+		@cart_item.customer_id = current_customer.id
+		@cart_item.save
+		redirect_to cart_items_path
 	end
 	
+	private
+	
+	def cart_item_params
+		params.require(:cart_item).permit(:item_id, :amount)
+	end
 end
