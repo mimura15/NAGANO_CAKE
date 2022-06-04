9d2f30a15d3efc41fa83d6767fae5bccee343f32
mimura15 <102932331+mimura15@users.noreply.github.com>
2022-06-04 13:36:29 +0000

    [Add] カート情報

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -16,7 +16,7 @@ class Public::CartItemsController < ApplicationController
 	end
 	
 	def create
-		@cart_item = Cart_item.new(cart_item_params)
+		@cart_item = CartItem.new(cart_item_params)
 		@cart_item.customer_id = current_customer.id
 		@cart_item.save
 		redirect_to cart_items_path
