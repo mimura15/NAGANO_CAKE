271e00c6ec1f98d6bbb6f3f9bea07682a69ccefb
mimura15 <102932331+mimura15@users.noreply.github.com>
2022-06-05 07:55:41 +0000

    [Add] destroy_all

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -5,7 +5,9 @@ class Public::CartItemsController < ApplicationController
 	end
 	
 	def update
-		
+		cart_item = CartItem.find(params[:id])
+		cart_item.update(cart_item_params)
+		redirect_to cart_items_path
 	end 
 	
 	def destroy
@@ -15,7 +17,8 @@ class Public::CartItemsController < ApplicationController
 	end
 	
 	def destroy_all
-		
+		CartItem.destroy_all
+		redirect_to cart_items_path
 	end
 	
 	def create
