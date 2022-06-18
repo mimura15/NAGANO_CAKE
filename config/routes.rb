a31c94c4322bd53130d6828ac2de4d1d68449410
mimura15 <102932331+mimura15@users.noreply.github.com>
2022-06-18 13:11:00 +0000

    [Add] order_detail controller

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
     resources :items, except: [:destroy]
     resources :customers, only: [:index, :show, :edit, :update]
     resources :orders, only: [:show, :update]
+    resources :order_details, only: [:update]
   end
  
  get '/admin' => 'admin/homes#top'
