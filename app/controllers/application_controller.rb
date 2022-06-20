40fdeb5a627c0ec3ebc7714445a3b0b378a07d78
mimura15 <102932331+mimura15@users.noreply.github.com>
2022-06-20 05:33:40 +0000

    [Fix] admin ログイン画面

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
 	
+	def after_sign_in_path_for(resource)
+	  if admin_signed_in?
+	    admin_path
+	  else
+	    flash[:notice] = "ようこそ、#{current_customer.last_name}さん！"
+	    root_path
+	  end
+	end
+	    
 
   protected
 
