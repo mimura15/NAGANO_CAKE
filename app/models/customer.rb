f8b9d9c18910ba760e6182124a44e4c9900b71a8
mimura15 <102932331+mimura15@users.noreply.github.com>
2022-06-21 12:34:13 +0000

    [Finish] admin

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -22,4 +22,12 @@ class Customer < ApplicationRecord
     super && (is_active == false)
   end
   
+  def full_name
+    last_name + ' ' + first_name
+  end
+  
+  def full_name_kana
+    last_name_kana + ' ' + first_name_kana
+  end
+  
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -4,6 +4,12 @@ class OrderDetail < ApplicationRecord
 	
 	enum making_status: { not_making: 0, preparation: 1, making: 2, making_complete: 3 }
 	
+	validates :item_id, presence: true
+	validates :order_id, presence: true
+	validates :price, presence: true
+	validates :amount, presence: true
+	validates :making_status, presence: true
+	
 	def subtotal
 		item.with_tax_price * amount
 	end
