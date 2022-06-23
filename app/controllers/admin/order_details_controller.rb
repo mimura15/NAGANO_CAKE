8e461885620ce1497ea68f73300931bc92dd9895
mimura15 <102932331+mimura15@users.noreply.github.com>
2022-06-23 11:30:02 +0000

    [Fix] ステータス連動

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -4,7 +4,9 @@ class Admin::OrderDetailsController < ApplicationController
 	def update
 		@order_detail = OrderDetail.find(params[:id])
 		@order_detail.update(order_detail_params)
-		if @order_detail.making_status == "making_complete"
+		@order = @order_detail.order
+		@order_details = @order.order_details
+		if @order_details.count == @order_details.where(making_status: "making_complete").count
 			@order_detail.order.update(status: "ready_to_ship")
 			redirect_to admin_order_path(@order_detail.order.id)
 		else
