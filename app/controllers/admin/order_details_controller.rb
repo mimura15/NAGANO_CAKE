7651f226908bd3bf609be9243ca2efd9618e922b
mimura15 <102932331+mimura15@users.noreply.github.com>
2022-06-23 02:10:46 +0000

    [Fix] about レイアウト

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -5,7 +5,6 @@ class Admin::OrderDetailsController < ApplicationController
 		@order_detail = OrderDetail.find(params[:id])
 		@order_detail.update(order_detail_params)
 		if @order_detail.making_status == "making_complete"
-			
 			@order_detail.order.update(status: "ready_to_ship")
 			redirect_to admin_order_path(@order_detail.order.id)
 		else
